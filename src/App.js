5bfb9811c4769efdf355cd9ed5f0119f6d16f8fc
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-09-15 23:48:48 +0700

    update config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layouts';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tu3tle</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+
+            const Page = route.component
+
+            let Layout = route.layout
+
+            if (route.layout === null) {
+              Layout = Fragment
+            } else {
+              Layout = route.layout
+            }
+
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            )
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
