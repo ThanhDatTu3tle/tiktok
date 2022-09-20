5091b5baffb0dd795eebd1890b27a44ce27a21ca
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-09-20 23:29:55 +0700

    update tippyjs to make search UI

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/324d3658d9f249c6de17426d23cc5206~c5_100x100.jpeg?x-expires=1663858800&x-signature=M7vGJteYc8hbrmmrWJ8vJ0FmpfU%3D" alt="Ngu"/>
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          Bơ Đáng Thương
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>bodangthuong</span>
+      </div>
+    </div>
+  )
+}
+
+export default AccountItem;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,13 +1,28 @@
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
-import images from '~/assets/images';
-
+import { useEffect, useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+import images from '~/assets/images';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+
+
 
 const cx = classNames.bind(styles)
 
 function Header() {
+
+  const [searchResult, setSearchResult] = useState([])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([])
+    }, 0)
+  }, [])
+
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
@@ -15,18 +30,35 @@ function Header() {
           <img src={images.logo} alt='Tiktok'/>
         </div>
 
-        <div className={cx('search')}>
-          <input className={cx('input')} placeholder='Search accounts and videos' spellCheck='false'/>
-          <button className={cx('clear-btn')}>
-            <img className={cx('clear-icon')} src={images.icon_clear} alt='clear'/>
-          </button>
-          <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
-          <span className={cx('span-divine')}></span>
-          <button className={cx('search-btn')}>
-            <img className={cx('search-icon')} src={images.icon_search} alt='search'/>
-          </button>
-        </div>
-
+        <Tippy 
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+              <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                <PopperWrapper>
+                  <h4 className={cx('search-title')}>
+                    Accounts
+                  </h4>
+                  <AccountItem />
+                  <AccountItem />
+                  <AccountItem />
+                </PopperWrapper>
+              </div>
+          )}
+        >
+          <div className={cx('search')}>
+            <input className={cx('input')} placeholder='Search accounts and videos' spellCheck='false'/>
+            <button className={cx('clear-btn')}>
+              <img className={cx('clear-icon')} src={images.icon_clear} alt='clear'/>
+            </button>
+            <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
+            <span className={cx('span-divine')}></span>
+            
+              <button className={cx('search-btn')}>
+                <img className={cx('search-icon')} src={images.icon_search} alt='search'/>
+              </button>
+          </div>
+        </Tippy>
         <div className={cx('actions')}>
           <h3>Upload</h3>
           <h3>Login</h3>
