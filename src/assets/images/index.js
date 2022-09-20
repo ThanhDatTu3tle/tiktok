0b853b2c19b31dc36407af548b091934842ec13c
MỸ NGÂN <myngan@MacBook-Air-cua-MY-NGAN.local>
2022-09-20 13:11:04 +0700

    UI navbar...

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    icon_clear: require('~/assets/images/icon_clear.svg').default,
+    icon_search: require('~/assets/images/icon_search.svg').default,
+}
+
+export default images;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles)
 
@@ -7,7 +11,26 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt='Tiktok'/>
+        </div>
 
+        <div className={cx('search')}>
+          <input className={cx('input')} placeholder='Search accounts and videos' spellCheck='false'/>
+          <button className={cx('clear-btn')}>
+            <img className={cx('clear-icon')} src={images.icon_clear} alt='clear'/>
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
+          <span className={cx('span-divine')}></span>
+          <button className={cx('search-btn')}>
+            <img className={cx('search-icon')} src={images.icon_search} alt='search'/>
+          </button>
+        </div>
+
+        <div className={cx('actions')}>
+          <h3>Upload</h3>
+          <h3>Login</h3>
+        </div>
       </div>
     </header>
   )
