e85e7c12f4b3f96e8523702ec6cf771ff0de8888
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-09-26 00:58:09 +0700

    update debounce for search

--- src/components/Layouts/components/Search/index.js
+++ src/components/Layouts/components/Search/index.js
@@ -8,6 +8,7 @@ import styles from './Search.module.scss';
 
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
+import { useDebounce } from '~/hooks';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { SearchIcon } from '~/components/Icons';
 
@@ -20,17 +21,19 @@ function Search() {
     const [showResult, setShowResult] = useState(true)
     const [loading, setLoading] = useState(false)
 
+    const debounced = useDebounce(searchValue, 500)
+
     const inputRef = useRef()
   
     useEffect(() => {
-      if(!searchValue.trim()) {
+      if(!debounced.trim()) {
         setSearchResult([])
         return;
       }
 
       setLoading(true)
 
-      fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+      fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
         .then(res => res.json())
         .then(res => {
           setSearchResult(res.data)
@@ -40,7 +43,7 @@ function Search() {
           setLoading(false)
         })
 
-    }, [searchValue])
+    }, [debounced])
 
     const handleHideResult = () => {
         setShowResult(false)
