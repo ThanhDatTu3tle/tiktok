37c0deaaa23bd897da4c577a1babc24c2a562f93
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-09-26 00:28:55 +0700

    update Header logic #2

--- src/components/Layouts/components/Search/index.js
+++ src/components/Layouts/components/Search/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 import classNames from 'classnames/bind';
@@ -18,14 +18,29 @@ function Search() {
     const [searchValue, setSearchValue] = useState('')
     const [searchResult, setSearchResult] = useState([])
     const [showResult, setShowResult] = useState(true)
+    const [loading, setLoading] = useState(false)
 
     const inputRef = useRef()
   
     useEffect(() => {
-      setTimeout(() => {
-        setSearchResult([1])
-      }, 0)
-    }, [])
+      if(!searchValue.trim()) {
+        setSearchResult([])
+        return;
+      }
+
+      setLoading(true)
+
+      fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        .then(res => res.json())
+        .then(res => {
+          setSearchResult(res.data)
+          setLoading(false)
+        })
+        .catch(() => {
+          setLoading(false)
+        })
+
+    }, [searchValue])
 
     const handleHideResult = () => {
         setShowResult(false)
@@ -42,7 +57,9 @@ function Search() {
                   <h4 className={cx('search-title')}>
                     Accounts
                   </h4>
-                  <AccountItem />
+                  {searchResult.map((result) => (
+                    <AccountItem key={result.id} data={result}/>
+                  ))}
                 </PopperWrapper>
               </div>
           )}
@@ -57,7 +74,7 @@ function Search() {
                 onChange={(e) => setSearchValue(e.target.value)}
                 onFocus={() => setShowResult(true)}
             />
-            {!!searchValue && (
+            {!!searchValue && !loading && (
                 <button 
                     className={cx('clear-btn')} 
                     onClick={() => { 
@@ -69,7 +86,7 @@ function Search() {
                     <img className={cx('clear-icon')} src={images.icon_clear} alt='clear'/>
                 </button>
             )}
-            {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/> */}
+            {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>}
             <span className={cx('span-divine')}></span>
             
             <button className={cx('search-btn')}>
